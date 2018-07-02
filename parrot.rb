92bd732c0bd5ea0136896b06c763fc1680001b9b
Shawna Davis <shawna_davis@alum.mit.edu>
2018-07-02 04:51:12 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string)
-  puts string
-  return string
+def parrot(string1)
+  puts "#{string1}"
+  return string1
 end
